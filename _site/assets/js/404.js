fbd1866643c2d7cdce3381560b251cee22d1dc4c
Isamu Thung <isamuthung@gmail.com>
2025-04-06 15:03:14 -0700

    Revert "Commit to change variable names for consistency"
    This reverts commit 8a9a48801742474b4572d9c55da25ba822d1c96b.

--- _site/assets/js/404.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const content = document.querySelector('.error-content');
-  content.style.opacity = 0;
-  content.style.transition = 'opacity 1s ease-in-out';
-
-  setTimeout(() => {
-    content.style.opacity = 1;
-  }, 100);
-});
